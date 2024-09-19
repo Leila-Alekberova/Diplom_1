1fd33fee0dbebce32f83463728183d5aa2014b54
alekberovalf <a-leilaalekberova@yandex.ru>
2024-09-19 18:03:19 +0300

    v2

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,14 +5,14 @@ import pytest
 from praktikum.bun import Bun
 
 class TestBun:
-    @pytest.mark.parametrize('bun_name, bun_price', [('Флюоресцентная булка', 988)])
-    def test_get_name(self, bun_name, bun_price):
-        bun = Bun(bun_name, bun_price)
-        name = bun.get_name()
-        assert bun_name == name
 
-    @pytest.mark.parametrize('bun_name, bun_price', [('Краторная булка', 1255)])
-    def test_get_price(self, bun_name, bun_price):
-        bun = Bun(bun_name, bun_price)
+    def test_get_name(self):
+        bun = Bun('Флюоресцентная булка', 988.0)
+        name = bun.get_name()
+        assert name == 'Флюоресцентная булка'
+
+
+    def test_get_price(self):
+        bun = Bun('Краторная булка', 1255.0)
         price = bun.get_price()
-        assert bun_price == price
+        assert price == 1255.0
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -7,12 +7,12 @@ class TestIngredient:
 
     # Тест на получение цены соуса
     def test_get_price(self):
-        price_sauces = Ingredient(INGREDIENT_TYPE_SAUCE, 'Соус Spicy-X', 90)
+        price_sauces = Ingredient(INGREDIENT_TYPE_SAUCE, 'Соус Spicy-X', 90.0)
         assert price_sauces.get_price() == 90
 
     # Тест на получение соуса
     def test_get_name(self):
-        name_sauces = Ingredient(INGREDIENT_TYPE_SAUCE,'Соус Spicy-X', 90)
+        name_sauces = Ingredient(INGREDIENT_TYPE_SAUCE,'Соус Spicy-X', 90.0)
         assert name_sauces.get_name() == 'Соус Spicy-X'
 
     # Тест на получение типа ингредиента (соус или начинка)
